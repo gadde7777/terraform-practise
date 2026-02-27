9f52819b5e7324dce5d585e20e77192f375eea38
an ga <rao2k42@gmail.com>
2026-02-27 10:50:09 -0500

    added

---
+++ ec2/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "Terraform"
+    Project = "Roboshop"
+      }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_all-terraform"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  # Ingress rule: Allow HTTP traffic from anywhere (IPv4)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP from anywhere"
+  }
+
+  # Egress rule: Allow all outbound traffic 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Represents all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+    Name = "allow_all-terraform"
+  }
+}
